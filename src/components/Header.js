3ef24419d42780434b5386c57745bc2b058c0dda
Caleb335 <belac335@gmail.com>
2020-11-19 12:47:00 +0100

    add class tranform properties

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,7 @@
-import React from "react"
+import React from 'react'
 
-const Header = ({title}) => {
-    return (
-        <div className="header" title={title}>
-
-        </div>
-    )
+const Header = ({ title }) => {
+  return <div className="header" title={title}></div>
 }
 
 export default Header
